d1e0035860aed368ac8bc8950bd4710ef0aab6ca
clauzio ohano <clauziio.ccosta@hotmail.com>
2017-04-25 16:07:37 -0300

    Add files via upload

---
+++ exemplo11_clausio.php
@@ -0,0 +1,16 @@
+***CLAUSIO OHANO ALVES 2&#176B"INFORMATICA***
+<html>
+<head>
+<meta charset="utf-8"/>
+</head>
+<body>
+<h1>estrutura de repeti&ccedil&atildeo(Do While)</h1>
+<?php
+$num=10;
+do{
+echo "numero:$num<br>";
+$num--;	
+}while($num>=1)
+?>
+</body>
+</html>
